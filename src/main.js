89631f28c2658abc9d33b4c838ca26e868a01cb8
Arnaud Valensi <arnaud.valensi@gmail.com>
2016-02-26 14:13:07 +0100

    fix lint

--- src/main.js
+++ src/main.js
@@ -9,15 +9,15 @@ import configureStore from './redux/configureStore';
 
 // Configure history for react-router
 const browserHistory = useRouterHistory(createBrowserHistory)({
-  basename: __BASENAME__
-})
+  basename: __BASENAME__,
+});
 
 // Create redux store and sync with react-router-redux. We have installed the
 // react-router-redux reducer under the key "router" in src/routes/index.js,
 // so we need to provide a custom `selectLocationState` to inform
 // react-router-redux of its location.
 const initialState = window.__INITIAL_STATE__;
-const store = configureStore(initialState, browserHistory)
+const store = configureStore(initialState, browserHistory);
 const history = syncHistoryWithStore(browserHistory, store, {
   selectLocationState: (state) => state.router,
 });
